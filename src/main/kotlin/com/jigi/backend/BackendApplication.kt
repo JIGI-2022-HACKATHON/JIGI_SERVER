884b48070964b55a6b78a89b57d450c2d211832e
jeayoung <k01066624566@gmail.com>
2022-10-06 17:06:28 +0900

    시간대 설정

--- src/main/kotlin/com/jigi/backend/BackendApplication.kt
+++ src/main/kotlin/com/jigi/backend/BackendApplication.kt
@@ -2,10 +2,14 @@ package com.jigi.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.PostMapping
+import java.time.LocalDateTime
+import java.util.*
 
 @SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
 	runApplication<BackendApplication>(*args)
 }
